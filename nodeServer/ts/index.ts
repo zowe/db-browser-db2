e2a3314ef9c63f31aa269a14a4f90366c4b344ed
Baoyuan Xing <bxing@rocketsoftware.com>
2018-11-15 15:53:28 -0500

    correct license issues Signed-off-by: Baoyuan Xing <bxing@rocketsoftware.com>

--- nodeServer/ts/index.ts
+++ nodeServer/ts/index.ts
@@ -1,3 +1,12 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
 
 import { Response, Request } from "express";
 import { Router } from "express-serve-static-core";
@@ -130,3 +139,13 @@ exports.DB2Router = function(context): Router {
     resolve(dataservice.getRouter());
   });
 }
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
